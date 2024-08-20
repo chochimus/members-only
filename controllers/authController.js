611c6aec4df690a967886251190cfbbf7245db41
chochimus <127354521+chochimus@users.noreply.github.com>
2024-08-20 18:04:03 -0400

    Creation of signup controller function and corresponding query

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,42 @@
 const { body, validationResult } = require("express-validator");
 const db = require("../db/queries");
 const bcrypt = require("bcryptjs");
-const asyncHandler = require("express-async-handler");
 
 const signupGet = (req, res) => {
   res.render("sign-up");
 };
 
-const signupPost = asyncHandler(async (req, res, next) => {
-  bcrypt.hash(req.body.password, 10, async(err, hashedPassword) => {
-    if (err) {
-        return next(err);
+const signupPost = async (req, res, next) => {
+  const { username, password } = req.body;
+
+  try {
+    const hashedPassword = await new Promise((resolve, reject) => {
+      bcrypt.hash(password, 10, (err, hash) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve(hash);
+      });
+    });
+
+    await db.insertUser({ username, password: hashedPassword });
+
+    // Redirect or respond with success
+    res.redirect("/login");
+  } catch (err) {
+    // Check if the error is a unique constraint violation
+    if (err.code === "23505") {
+      res.render("signup", {
+        error: "Username already taken. Please choose another one.",
+      });
+    } else {
+      // Handle other errors
+      next(err);
     }
-    await db.
-  });
-});
+  }
+};
+
+module.exports = {
+  signupGet,
+  signupPost,
+};
--- db/queries.js
+++ db/queries.js
@@ -1,1 +1,7 @@
 const pool = require("./pool");
+
+async function insertUser({ username, password }) {
+  const query = `INSERT INTO users (username, password) VALUES ($1, $2)`;
+  const values = [username, password];
+  await pool.query(query, values);
+}
