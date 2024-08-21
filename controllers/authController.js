24440e19df6bad0e3c7c4c956552cb45c196e019
chochimus <127354521+chochimus@users.noreply.github.com>
2024-08-21 18:05:46 -0400

    Complete sign up functionality with validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,35 +6,70 @@ const signupGet = (req, res) => {
   res.render("sign-up");
 };
 
-const signupPost = async (req, res, next) => {
-  const { username, password } = req.body;
+const signupValidate = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username required")
+    .isLength({ min: 3 })
+    .withMessage("Username must be at least 3 characters long")
+    .isLength({ max: 20 })
+    .withMessage("Username must be no more than 20 characters long")
+    .matches(/^[a-zA-Z0-9_]+$/)
+    .withMessage("Must only contain letters, numbers, or underscores")
+    .escape(),
+  body("password")
+    .notEmpty()
+    .withMessage("Password required")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters long")
+    .isLength({ max: 32 })
+    .withMessage("Password must be less than 32 characters long")
+    .matches(/[A-Z]/)
+    .withMessage("Password must contain at least one uppercase letter")
+    .matches(/[a-z]/)
+    .withMessage("Password must contain at least one lowercase letter")
+    .matches(/[0-9]/)
+    .withMessage("Password must contain at least one number")
+    .matches(/[\W_]/) // This regex checks for any special character
+    .withMessage("Password must contain at least one special character"),
+  body("confirm-password")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Passwords do not match"),
+];
 
-  try {
-    const hashedPassword = await new Promise((resolve, reject) => {
-      bcrypt.hash(password, 10, (err, hash) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(hash);
+const signupPost = [
+  signupValidate,
+  async (req, res, next) => {
+    const errors = await validationResult(req);
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      return res.status(400).render("sign-up", {
+        errors: errors.array(),
       });
-    });
-
-    await db.insertUser({ username, password: hashedPassword });
-
-    // Redirect or respond with success
-    res.redirect("/login");
-  } catch (err) {
-    // Check if the error is a unique constraint violation
-    if (err.code === "23505") {
-      res.render("sign-up", {
-        error: "Username already taken. Please choose another one.",
-      });
-    } else {
-      // Handle other errors
-      next(err);
     }
-  }
-};
+
+    const { username, password } = req.body;
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await db.insertUser({ username, password: hashedPassword });
+
+      // Redirect or respond with success
+      res.redirect("/login");
+    } catch (err) {
+      // Check if the error is a unique constraint violation
+      if (err.code === "23505") {
+        res.render("sign-up", {
+          error: "Username already taken. Please choose another one.",
+        });
+      } else {
+        // Handle other errors
+        next(err);
+      }
+    }
+  },
+];
 
 module.exports = {
   signupGet,
